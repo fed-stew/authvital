eba4cc461f5a3fd03ff303882482a46b8d16cea8
Gabrion Stewart <gabrion.stewart@interspark.com>
2026-04-24 08:09:15 +0900

    rewrote sdks

--- scripts/publish-packages.sh
+++ scripts/publish-packages.sh
@@ -4,13 +4,15 @@ set -e
 echo "Building packages..."
 npm run build -w @authvital/shared
 npm run build -w @authvital/contracts
-npm run build -w @authvital/node
-npm run build -w @authvital/react
+npm run build -w @authvital/core
+npm run build -w @authvital/browser
+npm run build -w @authvital/server
 
 echo "Publishing packages..."
 npm publish -w @authvital/shared --access public
 npm publish -w @authvital/contracts --access public
-npm publish -w @authvital/node --access public
-npm publish -w @authvital/react --access public
+npm publish -w @authvital/core --access public
+npm publish -w @authvital/browser --access public
+npm publish -w @authvital/server --access public
 
 echo "All packages published!"
